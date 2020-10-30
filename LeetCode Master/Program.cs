26de73db936f1ddce8ecd860b7b305627a8fc0b4
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-10-30 01:43:11 -0500

    Added RoundTestScores.cs

--- LeetCode Master/Program.cs
+++ LeetCode Master/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace LeetCode_Master
 {
     class Program
     {
+        public static object ArrayList { get; private set; }
+
         static void Main(string[] args)
         {
 
         }
     }
+
 }
